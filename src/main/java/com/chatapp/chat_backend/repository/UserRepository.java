c3332705208d1c210bfaa2276e695bc39bb3d390
Anand-Choudhary <anand.c.rishra@gmail.com>
2025-11-22 10:37:54 +0530

    Added JWT

--- src/main/java/com/chatapp/chat_backend/repository/UserRepository.java
+++ src/main/java/com/chatapp/chat_backend/repository/UserRepository.java
@@ -3,8 +3,12 @@ package com.chatapp.chat_backend.repository;
 import com.chatapp.chat_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long>
 {
+    Optional<User> findByUsername(String username);
 
 }
---
+++ src/main/java/com/chatapp/chat_backend/security/CustomUserDetailsService.java
@@ -0,0 +1,38 @@
+package com.chatapp.chat_backend.security;
+
+
+
+import com.chatapp.chat_backend.entity.User;
+import com.chatapp.chat_backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService
+{
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        if (!user.getIsActive()) {
+            throw new UsernameNotFoundException("User is inactive: " + username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
